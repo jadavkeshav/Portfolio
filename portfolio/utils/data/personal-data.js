b1bc8e22ecbaf2a4f9e405d90993ffbe73e6d7a2
jadavkeshav <jadavkeshav2005@gmail.com>
2024-11-02 14:45:08 +0530

    pic testing

--- portfolio/utils/data/personal-data.js
+++ portfolio/utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Jadav Keshav",
-  profile: '../../src/assets/images/profile-pic1.jpeg',
+  profile: '/profile-pic1.jpeg',
   designation: "Full Stack Developer",
   description: "I am Jadav Keshav, a Computer Science student at KMIT, Hyderabad, deeply passionate about software development. Proficient in MERN stack and Java, I thrive in crafting robust solutions. With strong problem-solving skills and a collaborative mindset, I am committed to delivering high-quality software. Eager to learn and grow, I seek opportunities to expand my expertise and make meaningful contributions to the tech industry as a Full Stack Developer.",
   email: 'jadavkeshav2005@gmail.com',
