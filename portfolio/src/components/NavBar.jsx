d199843bc89a277d706f2de16b38c735498c8bf7
jadavkeshav <jadavkeshav2005@gmail.com>
2025-06-23 15:55:55 +0530

    Added Blog

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -29,6 +29,11 @@ export default function NavBar() {
 						</Link>
 					</li>
 					<li>
+						<Link className="block px-4 py-2 no-underline outline-none hover:no-underline" to="https://blog.jadavkeshav.tech/">
+							<div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">BLOG</div>
+						</Link>
+					</li>
+					<li>
 						<Link className="block px-4 py-2 no-underline outline-none hover:no-underline" to="/#education">
 							<div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div>
 						</Link>
